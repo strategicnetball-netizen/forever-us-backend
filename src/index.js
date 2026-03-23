6ca50efd872d0a19843d5476e100e5d356977528
Forever Us Admin <admin@admin.com>
2026-03-23 10:58:58 +0700

    Add intimate preferences route to backend

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import admirersRoutes from './routes/admirers.js';
 import matchesRoutes from './routes/matches.js';
 import aiPicksRoutes from './routes/aiPicks.js';
 import usersRoutes from './routes/users.js';
+import intimatePreferencesRoutes from './routes/intimatePreferences.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -118,6 +119,10 @@ console.log('[STARTUP] AI picks routes loaded successfully');
 console.log('[STARTUP] Loading users routes...');
 app.use('/api/users', usersRoutes);
 console.log('[STARTUP] Users routes loaded successfully');
+
+console.log('[STARTUP] Loading intimate preferences routes...');
+app.use('/api/intimate-preferences', intimatePreferencesRoutes);
+console.log('[STARTUP] Intimate preferences routes loaded successfully');
 
 // 404 handler
 app.use((req, res) => {
